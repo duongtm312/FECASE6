695d7a6dc5dbcb3aa64cd3ef34a581f1ce73b897
duongtm312 <103014462+duongtm312@users.noreply.github.com>
2022-09-18 17:28:46 +0700

    notiuser

--- src/app/admin/navbartop/navbartop.component.ts
+++ src/app/admin/navbartop/navbartop.component.ts
@@ -36,7 +36,7 @@ export class NavbartopComponent  implements OnInit, OnChanges {
   }
 
   getAll() {
-    this.notificationService.getAll().subscribe((data) => {
+    this.notificationService.getAll('admin').subscribe((data) => {
       this.notification = data
       for (const n of data) {
         n.timeNotification = this.transform(n.timeNotification)
